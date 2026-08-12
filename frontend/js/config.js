c51135851194e624cb454d344e8b379db774e22a
TxilorAlvarez <jhontailora@gmail.com>
2026-08-12 17:45:14 -0500

    Corregir configuración de Supabase

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,21 +1,9 @@
-window.SUPABASE_URL =
-    "https://kqnscirhlhvqynaarxxr.supabase.co";
-
-window.SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_...";
-
-const SUPABASE_URL =
-    "https://kqnscirhlhvqynaarxxr.supabase.co";
-
-const SUPABASE_ANON_KEY =
-    "sb_publishable_okOR1lgzLj98YJKVsaMIZQ_2RFfbP-y";
-
 
 window.SUPABASE_URL =
     "https://kqnscirhlhvqynaarxxr.supabase.co";
 
 window.SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_okOR1lgzLj98YJKVsaMIZQ_2RFfbP-y";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxbnNjaXJobGh2cXluYWFyeHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MzMzMDAsImV4cCI6MjEwMjEwOTMwMH0.GmjhtwIS_TWr3NedWZGsvzMPZ2HpM4kbvIFGhGeH8NM";
 
 const SUPABASE_URL =
     window.SUPABASE_URL;
